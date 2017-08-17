b4426504605ef11b5a12233ebbcd0ec5e47f2d3e
Bryan Slatner <bsgit@slatner.com>
2017-08-17 13:05:02 -0400

    Add converted projects and solution file. Add batch file for building audacity dependencies.

---
+++ build/msw/build_audacity.cmd
@@ -0,0 +1,2 @@
+"C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild.exe" wx_dll.sln /p:Configuration="DLL Release" /p:Platform=Win32 /t:adv:Rebuild;base:Rebuild;core:Rebuild;html:Rebuild;net:Rebuild;qa:Rebuild;xml:Rebuild;wxexpat:Rebuild;wxjpeg:Rebuild;wxpng:Rebuild;wxregex:Rebuild;wxtiff:Rebuild;wxzlib:Rebuild
+
